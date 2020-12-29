5958f82b0e30b246c899e3ec71581ca151e11b41
57866696+thewooniverse@users.noreply.github.com <Joo@wooyoungs-mbp.home>
2020-12-29 23:58:22 +0000

    pyperclip + sysargv implementation + modified depth variable for style

--- note_taking_scripts/heading_generator.py
+++ note_taking_scripts/heading_generator.py
@@ -1,54 +1,60 @@
-# Heading generator creates different styles of headers and section dividers for note taking in Python for better organizing
+#! /usr/bin/env python3
+# heading_generator.py creates different styles of headers and section dividers for note taking in Python for better organizing
 # It takes a user input of a text string to be decorated, and a style.
 
+import pyperclip
+import sys
 
-# Style sheet
-separator = {'name': 'separator', 'depth': 5, 'width': 120, 'symbol': "-"}
-h1 = {'name': 'h1', 'depth': 3, 'width': 120, 'symbol': "*"}
-h2 = {'name': 'h2', 'depth': 3, 'width': 120, 'symbol': "#"}
 
+# Style sheet - this can later be ported out into a separate file
+separator = {'name': 'separator', 'depth': 3, 'width': 120, 'symbol': "-"}
+h1 = {'name': 'h1', 'depth': 1, 'width': 120, 'symbol': "*"}
+h2 = {'name': 'h2', 'depth': 1, 'width': 120, 'symbol': "#"}
 custom = {'name': "custom_style"}
 
 style_list = [separator, h1, h2]
 
 
-# define heading section
+# function definition
 def generate_heading(text, style):
     text_line = ("   " + text +
                  "   ").center(style['width'], style['symbol'])
-    depth_on_each_side = (style['depth'] - 1) // 2
-    if depth_on_each_side < 1:
+    if style['depth'] < 1:
         return ("\n\"\"\"\n" + text_line + "\n\"\"\"\n")
 
     depth_padding = ("".center(style['width'], style['symbol']) + '\n')
 
-    output = f"""\"\"\"\n{depth_padding * depth_on_each_side}{text_line}\n{depth_padding * depth_on_each_side}\"\"\"
+    output = f"""\"\"\"\n{depth_padding * style['depth']}{text_line}\n{depth_padding * style['depth']}\"\"\"
     """
 
     return output
 
 
-
 # user interaction
-print("Welcome to heading generator, here are the styles you have available: \n")
 
-for style in style_list:
-    print(f"{style.get('name')}: \nDepth: {style.get('depth')} \nWidth: {style.get('width')} \nSymbol: {style.get('symbol')} \nExample:\n" +
-          generate_heading("TEST", style) + ' \n')
+if len(sys.argv) < 2:
+    print('Usage: python heading_generator.py [style] - choose a style')
+    print('Below are some of the styles available in the default stylesheet')
+
+    for style in style_list:
+        print(f"{style.get('name')}: \nDepth: {style.get('depth')} \nWidth: {style.get('width')} \nSymbol: {style.get('symbol')} \nExample:\n" +
+              generate_heading("TEST", style) + ' \n')
+
+    print(
+        "if none of them suit your style, use the keyword 'custom' as the argument for [style] when calling the program")
+    sys.exit()
 
 
 txt_input = input("Please enter the text you would like to decorate: \n")
-style_input = input(
-    "Please enter the style you want this in, we'll create a new style if it doesn't exist: \n")
+style_input = sys.argv[1]  # a string is passed as stdin
 
-
-if style_input not in [style['name'] for style in style_list]:
+if style_input == 'custom':
     custom['name'] = style_input
     custom['depth'] = int(input(
-        "Please enter the depth of the header, ideally an odd number: "))
+        "Please enter the depth of padding on each side (above and below) the text: "))
     custom['width'] = int(input("Please enter the width of the header: "))
     custom['symbol'] = input("Please enter the symbol of the header: ")
-    style_input = custom
+    style_input = custom  # changes the style input to point to the dictionary
 
 
 else:
@@ -58,4 +64,10 @@ else:
 output = generate_heading(txt_input, style_input)
 
 
-print(output)
+print(output + '\n')
+print("copied to clipboard")
+pyperclip.copy(output)
+
+
+# user interaction
+# print("Welcome to heading generator, here are the styles you have available: \n")
