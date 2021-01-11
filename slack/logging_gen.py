2fb961ae10a81e55b4603b6fad25da7a3dd369af
TheWooniverse <57866696+thewooniverse@users.noreply.github.com>
2021-01-11 16:40:04 +0000

    add support for emoji in slack

--- slack/logging_gen.py
+++ slack/logging_gen.py
@@ -7,12 +7,13 @@ import datetime, pytz, sys, os, pyperclip, random, shutil
 # list of greetings
 sign_on_ls = ['Hello, Woo signing on!!', 'The Wooniverse is here for duty!', "I'm here n available!", 'Good morning team, Woo is online', 'Its Woo time!']
 sign_off_ls = ['Woo - out!', 'Woo signing off!! Bye Folks!!', 'Signing off - see you all tomorrow!']
+TU_specific_emoji =[':bling:', ':shimmy:', ':worm:', ':leftshark:', ':10-4:', ':robot:', ':ahhhh:']
+TU_specific_emoji += [':surge:', ':meow_party:', ':tarmac:', ':wavey:', ':tmnt:', ':wild:']
 
-# parse datetime now
+# parse datetime now and other vars
 now = datetime.datetime.now()
 format_now = now.strftime('%H:%M %B %d, %Y -- GMT+0')
-# print(format_now)
-
+random_emoji_seq = TU_specific_emoji[random.randint(0, len(TU_specific_emoji)-1)] * random.randint(1, 5)
 
 # define random sign on / sign off generator
 def str_gen(msg_type):
@@ -20,13 +21,13 @@ def str_gen(msg_type):
 
     if msg_type == "on":
 
-        result += f'{sign_on_ls[random.randint(0, len(sign_on_ls)-1)]}\n'
+        result += f'{random_emoji_seq} {sign_on_ls[random.randint(0, len(sign_on_ls)-1)]} {random_emoji_seq}\n'
         result += f"\nTime now is...\n{format_now}\n"
         result += "\nPlease check this thread for things I'll be working on and updates!"
 
     elif msg_type == "off":
 
-        result += f'{sign_off_ls[random.randint(0, len(sign_off_ls)-1)]}'
+        result += f'{random_emoji_seq} {sign_off_ls[random.randint(0, len(sign_off_ls)-1)]} {random_emoji_seq}'
         result += f"\nTime now is...{format_now}"
         result += f"\nPing me on Slack still if urgent!!"
 
@@ -41,8 +42,13 @@ pyperclip.copy(result)
 # detect filepath
 
 
-# log total hours... probably work with a .csv file.
+# log total hours... probably work with a .csv file locally
 
 
 
-# TODO: Add support for emoji
+# add gitignore.
+
+
+
+
+
